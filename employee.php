c2dc66a892b9516bb86d159942247be371efce86
Allen <42378086+allennithish@users.noreply.github.com>
2019-09-17 21:04:59 +0530

    Create employee.php

---
+++ employee.php
@@ -0,0 +1,27 @@
+<?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ class Employee extends CI_Controller { 
+ public function __construct()
+ {
+  parent::__construct();
+  $this->load->model('employee_model');
+  $this->load->helper(array('form', 'url'));
+ }
+ public function index()
+ {
+  $this->load->view('employee_form');
+ }
+ public function employee_form()
+ {
+  $save = array(
+      'emp_name'  => $this->input->post('emp_name'),
+      'emp_gender' => $this->input->post('emp_gender'),
+      'emp_email' => $this->input->post('emp_email'),
+      'emp_phone'  => $this->input->post('emp_phone'), 
+      'emp_address' => $this->input->post('emp_address') 
+       );
+
+  $this->employee_model->saveEmployee($save);
+  redirect('employee/index');
+ }
+}
